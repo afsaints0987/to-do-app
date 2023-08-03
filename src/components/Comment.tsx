cde8790bae8c74532bb862332218cff98b3cc76c
afsaints0987 <saints0987@gmail.com>
2023-08-03 19:25:44 +0800

    added function on comment component

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -3,17 +3,31 @@ import http from '../config/axios';
 
 const Comment: React.FC = () => {
     const [comments, setComments] = React.useState([]);
-
+    
+    const getComments = async () => {
+        const commentItem = await http.get('comments');
+        setComments(commentItem.data);
+    }
     React.useEffect(() => {
-        const getComments = async () => {
-            const commentItem = await http.get('comments');
-            console.log(commentItem.data);
-        }
         getComments();
     },[])
 
+    const handleCommentRefresh = React.useCallback(() => {
+        getComments();
+    },[])
+
+    handleCommentRefresh();
+
   return (
-    <div>Comment</div>
+    <div>
+        {
+            comments.map((comment: {id: number; comment_body: string}) => (
+                <div key={comment.id} className="container px-3 py-2 mt-2 bg-secondary rounded-3">
+                    <p className="text-sm text-light">{comment.comment_body}</p>
+                </div>
+            ))
+        }
+    </div>
   )
 }
 
